a2e5e379e508b3fd2292401f7dec596428fe834c
Devansh <devanshd78@gmail.com>
2026-03-30 19:48:23 +0530

    updates

--- src/app/brand/(auth)/signup/BrandSignupclient.tsx
+++ src/app/brand/(auth)/signup/BrandSignupclient.tsx
@@ -636,7 +636,7 @@ export default function BrandSignupPage() {
                             Terms of Service
                           </Link>{" "}
                           and{" "}
-                          <Link href="/privacy" className="font-semibold hover:underline text-current">
+                          <Link href="/privacy-policy" className="font-semibold hover:underline text-current">
                             Privacy Policy
                           </Link>
                         </span>
